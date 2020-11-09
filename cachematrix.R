dc059170f589536fbbbbf9ec6c33ebe5532356a0
SANIAL DAS <53612010+sanial2001@users.noreply.github.com>
2020-11-09 10:55:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## first function is used for calculating matrix thriugh lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  
+  list(set = set , get = get ,
+       setinv = setinv ,
+       getinv = getinv
+       )
 
 }
 
 
-## Write a short comment describing this function
+## this is used for caching the data
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    print("caching data from cache")
+    return(inv)
+  }
+  
+  value <- x$get()
+  inv <- solve(value,...)
+  x$setinv(inv)
+  inv
+  
 }
